b160cfa7910f464f6517f21225cb01b7e71cc430
Halalisani <halalisani@iamsignific.com>
2021-01-19 17:52:47 +0200

    fix: server listen port

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
